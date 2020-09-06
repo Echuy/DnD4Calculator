ffb62c9d267390b85a6ca24af5d67a9dfba26af1
Echuy <echuy@outlook.com>
2020-09-06 15:10:18 +0300

    Добавьте файлы проекта.

---
+++ Models/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WnGCalculator.Models
+{
+    public class DnDObject
+    {
+        public int id { get; set; }
+        public Tag Type { get; set;}
+    }
+
+    public class Tag
+    {
+        public int id {get;}
+
+        public int ParentTag { get; set; }
+        public string Name { get; set; }
+
+        public string SysName { get; set; }
+
+        public string ImageName { get; set; }
+
+    }
+}
